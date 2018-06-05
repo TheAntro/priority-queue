707500e426cabe2ea3da25cb7548348b9f4927ae
Valtteri Ranta <waltherbeach@gmail.com>
2018-06-05 21:53:51 +0300

    Project initialization

---
+++ main.cc
@@ -0,0 +1,14 @@
+/* Created by Valtteri Ranta, GitHub: waltherbeach on 5th of June 2018
+ * Will implement a priority queue using a vector, a linked list, a chunklist, and a heap
+ * Will also facilitate performance measurements for the implementations
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
