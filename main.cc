e6be8fd0dc4a296428ee42c133ce818ce2c8cbd2
Valtteri Ranta <waltherbeach@gmail.com>
2018-06-10 21:29:32 +0300

    Project finalized

--- main.cc
+++ main.cc
@@ -1,5 +1,9 @@
-/* Created by Valtteri Ranta, GitHub: waltherbeach on 5th of June 2018
- * Will implement a priority queue using a vector, a linked list, a chunklist, and a heap
+/* Created by Valtteri Ranta, GitHub: waltherbeach on 5th of June 2018, completed 10th of June 2018
+ * Implements a priority queue using a vector, a linked list, a chunklist, and a heap
+ * Current implementation uses a heap. Other implementations are included in the repository and need to be
+ * added to the .pro file in place of pqheap.cc to take into use. Also: replace the private implementation
+ * of pqueue.hh for the appropriate version
+ *
  * Will not facilitate performance measurements for the implementations
  */
 
